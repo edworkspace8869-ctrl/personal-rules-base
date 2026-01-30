858d3b4fa47a72edb5e13807ecef45e350a10753
edworkspace8869-ctrl <edworkspace8869@gmail.com>
2026-01-30 17:52:30 +0800

    Revert "Update service-worker.js"
    This reverts commit 8153651e5dbac99b7e591082aa8be7518f97f9cf.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Handles caching and offline functionality
  */
 
-const CACHE_NAME = 'personal-rules-base-alpha-v1.1';
+const CACHE_NAME = 'personal-rules-base-alpha-v1';
 const urlsToCache = [
   '/personal-rules-base/',
   '/personal-rules-base/index.html',
